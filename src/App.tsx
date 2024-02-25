f1a299febad55b82c54ce3931d58a838d2f8add6
Sin-god <illya.komarnickii1@gmail.com>
2024-02-25 09:50:03 +0200

    Added absolute pathes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { CssBaseline } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Admin, DataProvider, Loading, Resource } from "react-admin";
 import buildHasuraProvider from "ra-data-hasura";
-import { productsResource } from "./modules/products/products-resource";
+import { productsResource } from "@app/modules/products/products-resource";
 
 export const App = () => {
   const [dataProvider, setDataProvider] = useState<DataProvider<string> | null>(
