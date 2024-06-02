38195050d41925bc76a4b0f0b4f4726ac9930cb5
Sin-god <illya.komarnickii1@gmail.com>
2024-06-02 11:32:13 +0300

    add import

--- src/modules/services/components/services-create/services-create.component.tsx
+++ src/modules/services/components/services-create/services-create.component.tsx
@@ -1,5 +1,7 @@
 import { CloudinaryInput } from "@app/common/components/cloudinary-input/cloudinary-input.comnponent";
 import { Create, SimpleForm, TextInput } from "react-admin";
+import { RichTextInput } from "ra-input-rich-text";
+
 
 export const ServicesCreate = () => (
   <Create title="Додати послугу">
