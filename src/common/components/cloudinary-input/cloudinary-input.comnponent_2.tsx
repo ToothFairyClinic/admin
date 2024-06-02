2226fac98610d0ac9b6ac025979d06710cb42fbd
Sin-god <illya.komarnickii1@gmail.com>
2024-06-02 11:58:52 +0300

    hot fix

---
+++ src/common/components/cloudinary-input/cloudinary-input.comnponent_2.tsx
@@ -0,0 +1,59 @@
+import { FC } from "react";
+import { InputProps, useInput, useNotify } from "react-admin";
+import axios, { AxiosError } from "axios";
+import { CloudinaryInputUI } from "../cloudinary-input-ui/cloudinary-input-ui.components";
+import { CloudinaryUploadDTO } from "../cloudinary-input-ui/cloudinary-upload.dto";
+import { useCloudinarySignatureQuery } from "@app/core/types";
+
+export const CloudinaryInput_2: FC<InputProps> = (props) => {
+  const { label, source } = props;
+
+  const computetLabel = String(label) ?? source;
+  const { data: cloudSignature, loading } = useCloudinarySignatureQuery({
+    fetchPolicy: "network-only",
+  });
+
+  const notify = useNotify();
+
+  const {
+    field: { onChange, value },
+  } = useInput(props);
+
+  const onImageSelected = async (image: File) => {
+    if (!cloudSignature?.cloudinarySignature) {
+      return;
+    }
+    const { cloudName, apiKey, publicId, signature, timestamp } =
+      cloudSignature.cloudinarySignature;
+
+    const url = `https://api.cloudinary.com/v1_1/${cloudName}/auto/upload`;
+
+    const formData = new FormData();
+
+    formData.append("file", image);
+    formData.append("api_key", apiKey);
+    formData.append("public_id", publicId);
+    formData.append("signature", signature);
+    formData.append("timestamp", String(timestamp));
+    formData.append("folder", "products");
+    formData.append("upload_preset", "nibw5oyz");
+
+    let data: CloudinaryUploadDTO;
+    try {
+      ({ data } = await axios.post<CloudinaryUploadDTO>(url, formData));
+    } catch (e) {
+      notify((e as AxiosError).message);
+    }
+
+    onChange(data!.public_id);
+  };
+  return (
+    <CloudinaryInputUI
+      label={computetLabel}
+      value={value}
+      disabled={loading}
+      onImageSelected={onImageSelected}
+      fitImage={true}
+    />
+  );
+};
--- src/modules/services/components/services-create/services-create.component.tsx
+++ src/modules/services/components/services-create/services-create.component.tsx
@@ -1,6 +1,7 @@
 import { CloudinaryInput } from "@app/common/components/cloudinary-input/cloudinary-input.comnponent";
 import { Create, SimpleForm, TextInput } from "react-admin";
 import { RichTextInput } from "ra-input-rich-text";
+import { CloudinaryInput_2 } from "@app/common/components/cloudinary-input/cloudinary-input.comnponent_2";
 
 
 export const ServicesCreate = () => (
@@ -9,6 +10,7 @@ export const ServicesCreate = () => (
       <TextInput source="name" fullWidth label="Назва" />
       <RichTextInput source="description" fullWidth label="Опис" />
       <CloudinaryInput source="image" label="Фото" />
+      <CloudinaryInput_2 source="image" label="Фото" />
     </SimpleForm>
   </Create>
 );
