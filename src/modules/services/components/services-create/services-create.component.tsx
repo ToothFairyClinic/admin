4ef67ee15418b62d428c0ee2c9b11d32de7ec6d6
Illya Komarnitskyi <illya.komarnickii1@gmail.com>
2026-07-28 17:38:19 +0300

    feat: add admin dashboard index template and services create/edit components

--- src/modules/services/components/services-create/services-create.component.tsx
+++ src/modules/services/components/services-create/services-create.component.tsx
@@ -1,5 +1,5 @@
 import { CloudinaryInput } from "@app/common/components/cloudinary-input/cloudinary-input.comnponent";
-import { Create, SimpleForm, TextInput } from "react-admin";
+import { Create, ReferenceInput, SelectInput, SimpleForm, TextInput } from "react-admin";
 import { RichTextInput } from "ra-input-rich-text";
 
 
@@ -9,12 +9,29 @@ export const ServicesCreate = () => (
       <TextInput source="name" fullWidth label="Назва" />
       <TextInput source="name_en" fullWidth label="Назва en" />
       <RichTextInput source="description" fullWidth label="Опис" />
+      <RichTextInput source="description_en" fullWidth label="Опис en" />
       <CloudinaryInput source="image" label="Фото іконки" />
       <CloudinaryInput source="mainImage" label="Фото послуги" />
       <TextInput source="seo_title" fullWidth label="SEO title ua" />
       <TextInput source="seo_description" multiline fullWidth label="SEO Description ua" />
       <TextInput source="seo_title_en" fullWidth label="SEO title en" />
       <TextInput source="seo_description_en" multiline fullWidth label="SEO Description en" />
+
+      <ReferenceInput
+        source="parent_id"
+        reference="services"
+      // filter={{ parent_id: { _is_null: true } }}
+      >
+        <SelectInput
+          optionText="name"
+          label="Батьківська послуга"
+          fullWidth
+          emptyText="Немає (Коренева послуга)"
+        />
+      </ReferenceInput>
+
+      <TextInput source="slug" multiline fullWidth label="Slug" />
+      <TextInput source="slug_en" multiline fullWidth label="Slug en" />
     </SimpleForm>
   </Create>
 );
