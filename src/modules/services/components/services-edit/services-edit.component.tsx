2b818b47be60ba57040821f1e78e0955faa7aa0d
Illya Komarnitskyi <illya.komarnickii1@gmail.com>
2026-08-24 16:30:56 +0300

    hot fix

--- src/modules/services/components/services-edit/services-edit.component.tsx
+++ src/modules/services/components/services-edit/services-edit.component.tsx
@@ -12,10 +12,9 @@ export const ServicesEdit = () => {
         <RichTextInput source="description" fullWidth label="Опис" />
         <RichTextInput source="description_en" fullWidth label="Опис en" />
         <CloudinaryInput source="image" label="Фото іконки" />
-
         <CloudinaryInput source="mainImage" label="Фото послуги" />
-        <CloudinaryInput source="image_alt" label="Alt текст" />
-        <CloudinaryInput source="image_alt_en" label="Alt текст en" />
+        <TextInput source="image_alt" label="Alt текст" />
+        <TextInput source="image_alt_en" label="Alt текст en" />
 
         <ArrayInput source="faq_ua" label="Часті питання (FAQ UA)" fullWidth>
           <SimpleFormIterator inline>
